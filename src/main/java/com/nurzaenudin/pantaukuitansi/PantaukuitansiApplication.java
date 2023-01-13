65bc2bb5a22bffb53e893358000824db954d441a
Nur Zaenudin <nurzaenudin@gmail.com>
2023-01-13 14:37:08 +0700

    pertama

---
+++ src/main/java/com/nurzaenudin/pantaukuitansi/PantaukuitansiApplication.java
@@ -0,0 +1,13 @@
+package com.nurzaenudin.pantaukuitansi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantaukuitansiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PantaukuitansiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nurzaenudin/pantaukuitansi/PantaukuitansiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurzaenudin.pantaukuitansi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantaukuitansiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
