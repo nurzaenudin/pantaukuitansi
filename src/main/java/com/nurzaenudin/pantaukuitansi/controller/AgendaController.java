7093744adf2cb84ffa54bf4ff103d7ae310d9057
Nur Zaenudin <nurzaenudin@gmail.com>
2023-01-20 16:38:40 +0700

    menambah controller hapus dan update agenda

--- src/main/java/com/nurzaenudin/pantaukuitansi/controller/AgendaController.java
+++ src/main/java/com/nurzaenudin/pantaukuitansi/controller/AgendaController.java
@@ -6,12 +6,16 @@ package com.nurzaenudin.pantaukuitansi.controller;
 
 import com.nurzaenudin.pantaukuitansi.dao.AgendaDao;
 import com.nurzaenudin.pantaukuitansi.entity.Agenda;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -43,6 +47,29 @@ public class AgendaController {
         agendadao.save(agenda);
     }
     
+    @DeleteMapping("/agenda/{id}")
+    @ResponseBody
+    public String hapusAgenda(@PathVariable Long id){
+        agendadao.deleteById(id);
+        return "hapus berhasil";
+    }
     
+    @GetMapping ("/agenda/{id}")
+    @ResponseBody
+    public Optional getAgenda (@PathVariable Long id){
+        return agendadao.findById(id);
+    }
+    
+    
+    @PutMapping("agenda/{id}")
+    @ResponseBody
+    public void updateAgenda (@RequestBody Agenda agenda, @PathVariable Long id){
+        Optional <Agenda> ag=getAgenda(id);
+        if(!ag.isPresent())
+            return;
+        agenda.setId(id);
+        agendadao.save(agenda);
+        System.out.println("berhasil simpan agenda nomor "+ agenda.getId());
+    }
     
 }
