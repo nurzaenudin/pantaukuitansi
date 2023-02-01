8a6acf97e52d8a1e99419524e2ac819b5201fe22
Nur Zaenudin <nurzaenudin@gmail.com>
2023-02-01 15:30:17 +0700

    disable CORS pada get Perintah Bayar

--- src/main/java/com/nurzaenudin/pantaukuitansi/controller/PerintahBayarController.java
+++ src/main/java/com/nurzaenudin/pantaukuitansi/controller/PerintahBayarController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,6 +38,7 @@ public class PerintahBayarController {
     }
     
     @GetMapping("perintahbayar/all")
+    @CrossOrigin(origins = "*")
     @ResponseBody
     public Page<PerintahBayar> getAllPerintahBayars(Pageable pageable){
         return perintahbayardao.findAll(pageable);
